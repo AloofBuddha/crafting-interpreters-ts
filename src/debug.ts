08fb90c68b673a23f93a1b4804457b43eae8ffa9
Benjamin Cohen <bac1087@gmail.com>
2020-04-06 11:37:53 -0500

    updated with base project settings

--- src/debug.ts
+++
@@ -1,8 +0,0 @@
-const debug = debugPrint.bind(null, process.env.DEBUG === "interpreter");
-
-function debugPrint(enabled = false, ...args: any[]) {
-  if (enabled) console.log(...args);
-}
-
-export { debugPrint };
-export default debug;
